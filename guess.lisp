35266743d16d42e03894dcf89cab24350b9b0783
pagu1003 <pagu1003@student.miun.se>
2022-08-15 17:03:40 +0200

    beta version ready ;)

--- guess.lisp
+++ guess.lisp
@@ -1,2 +1,29 @@
-(print "test")
-(print "test2")
+(setf *random-state* (make-random-state t))
+(defvar seed (random 3))
+(defvar thenumber (+ seed 1))
+(defvar stopit 0)
+(defun testnumber(n)(if(= n thenumber)
+                               (guessiscorrect)
+							   (guessisnotcorrect n)
+							   )
+							   )
+							   
+(defun guessisnotcorrect(n)(if(> n thenumber)
+                               (print "lower")
+							   (print "higher"))
+							   (setq stopit 0))
+(defun guessiscorrect()(print "correct")(setq stopit 1))
+	                                     
+
+(loop 
+do (print "Guess the number: ")
+(setq guess(read))
+(testnumber guess)
+
+
+
+
+
+until (= stopit 1)
+
+)
